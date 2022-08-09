8ba53e3406681545a82dcd1f2e77966cd25922bb
Jaydeep Chakrabarty <jaydeepc@in-Jaydeepc-3089.local>
2022-08-09 12:34:22 +0530

    JD: Added few css based in new designs

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,8 @@ timeline
     transformOrigin: '10% 50%',
     ease: 'sine.out',
   })
-  .from('.start-text', { x: -600, ease: 'bounce' })
+  .from('.start-text', { x: -800, ease: 'bounce' })
+  .from('.play-video', { x: -800, ease: 'bounce' })
   .from('.wheel-container', { y: -1700 })
   .from('.why-core', { y: -1700 })
   .from('.cone1', { y: -1700 })
