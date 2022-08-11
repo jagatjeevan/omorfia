acbd24b32e8177c9b84050f4ecff93a1c87c79e1
Jaydeep Chakrabarty <jaydeepc@in-Jaydeepc-3089.local>
2022-08-11 17:26:47 +0530

    JD: Added all lenses svg and animations

--- src/js/script1.js
+++ src/js/script1.js
@@ -32,6 +32,34 @@ gsap
   .delay(2.4);
 
 gsap
+  .from('.lense-revolutionizeTech', {
+    opacity: 0,
+    duration: 0.5,
+  })
+  .delay(2.8);
+
+gsap
+  .from('.lense-awesomePartner', {
+    opacity: 0,
+    duration: 0.5,
+  })
+  .delay(3.2);
+
+gsap
+  .from('.lense-vibrantCommunity', {
+    opacity: 0,
+    duration: 0.5,
+  })
+  .delay(3.6);
+
+gsap
+  .from('.lense-commercialSuccess', {
+    opacity: 0,
+    duration: 0.5,
+  })
+  .delay(4);
+
+gsap
   .from('.start-text', {
     duration: 1.3,
     x: -200,
@@ -41,11 +69,11 @@ gsap
 
 //Button click animation
 $('.play-video').on('click', function () {
-  gsap.set('.video-area', { display: 'flex', duration: 1.3 }).delay(1.2);
   gsap.to('.start-text', {
     // opacity: 0,
     y: -200,
     duration: 1,
   });
   gsap.fromTo('.play-video', { opacity: 1 }, { opacity: 0, duration: 1.1 });
+  gsap.fromTo('.video-area', { opacity: 0 }, { display: 'flex', duration: 2, opacity: 1 });
 });
