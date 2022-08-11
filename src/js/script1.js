3dc0abb1f4fdb7a436fcec84da760cb4d331f256
Jaydeep Chakrabarty <jaydeepc@in-Jaydeepc-3089.local>
2022-08-11 13:38:11 +0530

    JD: Added animation for len wheel

--- src/js/script1.js
+++ src/js/script1.js
@@ -4,7 +4,7 @@ gsap.to('#screen1', {
 });
 
 gsap
-  .from('.backgruond-dots', {
+  .from('.background-image', {
     opacity: 0,
     duration: 0.5,
   })
@@ -18,9 +18,28 @@ gsap
   .delay(1.8);
 
 gsap
+  .from('.lenses-core', {
+    opacity: 0,
+    duration: 0.5,
+  })
+  .delay(2);
+
+gsap
+  .from('.lense-positiveSocialChange', {
+    opacity: 0,
+    duration: 0.5,
+  })
+  .delay(2.4);
+
+gsap
   .from('.start-text', {
     duration: 1.3,
     x: -200,
     opacity: 0,
   })
   .delay(1.3);
+
+//Button click animation
+$('.play-video').on('click', function () {
+  gsap.set('.video-area', { display: 'flex', duration: 1.3 }).delay(1.2);
+});
