195f40c83b02e7e27b4e267373fa11774c813a11
Keerthivasan D <keerthivasan.d@thoughtworks.com>
2022-08-18 13:14:41 +0530

    Keerthivasan: Add exit transition for main screen

--- src/js/script.js
+++ src/js/script.js
@@ -67,7 +67,7 @@ gsap
   })
   .delay(1.3);
 
-//Button click animation
+// Button click animation
 $('.play-video').on('click', function () {
   gsap.to('.start-text', {
     // opacity: 0,
@@ -86,3 +86,59 @@ $('#close-video').on('click', function () {
   gsap.fromTo('.play-video', { opacity: 0, duration: 1.1 }, { opacity: 1 });
   gsap.fromTo('.video-area', { display: 'flex', duration: 2, opacity: 1 }, { opacity: 0 });
 });
+
+// Exit transition
+const pageTransition = () => {
+  gsap.to('.start-text', {
+    duration: 1.3,
+    x: -200,
+    opacity: 0,
+  });
+  gsap.to('.lense-commercialSuccess-back', {
+    zIndex: 5,
+    duration: 0,
+  });
+  gsap.to('.lense-commercialSuccess-back', {
+    opacity: 1,
+    duration: 1.5,
+  });
+  gsap
+    .to(
+      `.lense-positiveSocialChange,
+        .lense-revolutionizeTech,
+        .lense-awesomePartner,
+        .lense-vibrantCommunity,
+        .lense-commercialSuccess,
+        .lenses-core,
+        .background-image,
+        .skeleton`,
+      {
+        duration: 0.5,
+        opacity: 0,
+      },
+    )
+    .delay(0.8);
+  gsap
+    .to('.lense-commercialSuccess-back', {
+      duration: 0.8,
+      left: '-158.8%',
+      width: '10%',
+      top: '-4.5%',
+    })
+    .delay(1.3);
+  gsap
+    .to('#screen1', {
+      duration: 1,
+      backgroundColor: '#ECF1F3',
+    })
+    .delay(1.3);
+  gsap
+    .to('#screen1', {
+      duration: 0.1,
+      display: 'none',
+    })
+    .delay(2.3);
+  // load new screen
+};
+
+$('.lense-commercialSuccess').on('click', pageTransition);
