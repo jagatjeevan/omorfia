3fb127a8445f769710ff61f663f2ee65debf7799
Keerthivasan D <keerthivasan.d@thoughtworks.com>
2022-08-18 20:33:01 +0530

    Keerthivasan: Refactor gsap code

--- src/js/script.js
+++ src/js/script.js
@@ -1,71 +1,319 @@
-gsap.to('#screen1', {
-  duration: 1.3,
-  backgroundColor: '#003d4f',
+// Screen 1 Transitions
+const loadScreen1 = () => {
+  gsap.fromTo(
+    '#screen1',
+    {
+      duration: 0.1,
+      display: 'none',
+    },
+    {
+      duration: 0.1,
+      display: 'flex',
+    },
+  );
+  gsap.fromTo(
+    '#screen1',
+    {},
+    {
+      duration: 1.3,
+      backgroundColor: '#003d4f',
+    },
+  );
+
+  gsap
+    .fromTo(
+      '.background-image',
+      {
+        opacity: 0,
+        duration: 0.5,
+      },
+      {
+        opacity: 0.1,
+        duration: 0.5,
+      },
+    )
+    .delay(1.3);
+
+  gsap
+    .fromTo(
+      '.skeleton',
+      {
+        opacity: 0,
+        duration: 0.5,
+      },
+      {
+        opacity: 1,
+        duration: 0.5,
+      },
+    )
+    .delay(1.8);
+
+  gsap
+    .fromTo(
+      '.lenses-core',
+      {
+        opacity: 0,
+        duration: 0.5,
+      },
+      {
+        opacity: 2,
+        duration: 0.5,
+      },
+    )
+    .delay(2);
+
+  gsap
+    .fromTo(
+      '.lense-positiveSocialChange',
+      {
+        opacity: 0,
+        duration: 0.5,
+      },
+      {
+        opacity: 1,
+        duration: 0.5,
+      },
+    )
+    .delay(2.4);
+
+  gsap
+    .fromTo(
+      '.lense-revolutionizeTech',
+      {
+        opacity: 0,
+        duration: 0.5,
+      },
+      {
+        opacity: 1,
+        duration: 0.5,
+      },
+    )
+    .delay(2.8);
+
+  gsap
+    .fromTo(
+      '.lense-awesomePartner',
+      {
+        opacity: 0,
+        duration: 0.5,
+      },
+      {
+        opacity: 1,
+        duration: 0.5,
+      },
+    )
+    .delay(3.2);
+
+  gsap
+    .fromTo(
+      '.lense-vibrantCommunity',
+      {
+        opacity: 0,
+        duration: 0.5,
+      },
+      {
+        opacity: 1,
+        duration: 0.5,
+      },
+    )
+    .delay(3.6);
+
+  gsap
+    .fromTo(
+      '.lense-commercialSuccess',
+      {
+        opacity: 0,
+        duration: 0.5,
+      },
+      {
+        opacity: 1,
+        duration: 0.5,
+      },
+    )
+    .delay(4);
+
+  gsap
+    .fromTo(
+      '.start-text',
+      {
+        duration: 1.3,
+        x: -200,
+        opacity: 0,
+      },
+      {
+        duration: 1.3,
+        x: 0,
+        opacity: 1,
+      },
+    )
+    .delay(1.3);
+  $('.lense-commercialSuccess-back').css({
+    zIndex: -1,
+    opacity: 0,
+  });
+};
+const exitScreen1 = () => {
+  gsap.fromTo(
+    '.start-text',
+    {
+      duration: 1.3,
+      x: 0,
+      opacity: 1,
+    },
+    {
+      duration: 1.3,
+      x: -200,
+      opacity: 0,
+    },
+  );
+  gsap.fromTo(
+    '.lense-commercialSuccess-back',
+    {
+      zIndex: -1,
+      duration: 0,
+    },
+    {
+      zIndex: 5,
+      duration: 0,
+    },
+  );
+  gsap.fromTo(
+    '.lense-commercialSuccess-back',
+    {
+      opacity: 0,
+      duration: 1.5,
+    },
+    {
+      opacity: 1,
+      duration: 1.5,
+    },
+  );
+  gsap
+    .fromTo(
+      `.lense-positiveSocialChange,
+        .lense-revolutionizeTech,
+        .lense-awesomePartner,
+        .lense-vibrantCommunity,
+        .lense-commercialSuccess,
+        .lenses-core,
+        .background-image,
+        .skeleton`,
+      {},
+      {
+        duration: 0.5,
+        opacity: 0,
+      },
+    )
+    .delay(0.8);
+  gsap
+    .fromTo(
+      '.lense-commercialSuccess-back',
+      {
+        duration: 0.8,
+        width: '93%',
+        left: '50%',
+        top: '50%',
+      },
+      {
+        duration: 0.8,
+        left: '-103.5%',
+        width: '8%',
+        top: '6.25%',
+      },
+    )
+    .delay(1.3);
+  gsap
+    .fromTo(
+      '#screen1',
+      {},
+      {
+        duration: 1,
+        backgroundColor: '#ECF1F3',
+      },
+    )
+    .delay(1.3);
+  gsap
+    .fromTo(
+      '#screen1',
+      {
+        duration: 0.1,
+        display: 'flex',
+      },
+      {
+        duration: 0.1,
+        display: 'none',
+      },
+    )
+    .delay(2.3);
+};
+
+// Commercial Success Transitions
+const loadCommercialSuccess = () => {
+  gsap.fromTo(
+    '#commercial-success',
+    {
+      duration: 0.1,
+      display: 'none',
+    },
+    {
+      duration: 0.1,
+      display: 'block',
+    },
+  );
+  gsap
+    .fromTo(
+      '.back-icon-button',
+      {
+        duration: 0.8,
+        opacity: 0,
+      },
+      {
+        duration: 0.8,
+        opacity: 1,
+      },
+    )
+    .delay(2.3);
+  gsap
+    .fromTo(
+      '.description',
+      {
+        duration: 1,
+        x: -100,
+        opacity: 0,
+      },
+      {
+        duration: 1,
+        x: 0,
+        opacity: 1,
+      },
+    )
+    .delay(2.3);
+};
+const exitCommercialSuccess = () => {
+  gsap.fromTo(
+    '#commercial-success',
+    {
+      duration: 0.1,
+      display: 'block',
+    },
+    {
+      duration: 0.1,
+      display: 'none',
+    },
+  );
+};
+
+$('.lense-commercialSuccess').on('click', () => {
+  exitScreen1();
+  loadCommercialSuccess();
+});
+$('#back-icon-button').on('click', () => {
+  console.log('hi');
+  exitCommercialSuccess();
+  loadScreen1();
 });
 
-gsap
-  .from('.background-image', {
-    opacity: 0,
-    duration: 0.5,
-  })
-  .delay(1.3);
-
-gsap
-  .from('.skeleton', {
-    opacity: 0,
-    duration: 0.5,
-  })
-  .delay(1.8);
-
-gsap
-  .from('.lenses-core', {
-    opacity: 0,
-    duration: 0.5,
-  })
-  .delay(2);
-
-gsap
-  .from('.lense-positiveSocialChange', {
-    opacity: 0,
-    duration: 0.5,
-  })
-  .delay(2.4);
-
-gsap
-  .from('.lense-revolutionizeTech', {
-    opacity: 0,
-    duration: 0.5,
-  })
-  .delay(2.8);
-
-gsap
-  .from('.lense-awesomePartner', {
-    opacity: 0,
-    duration: 0.5,
-  })
-  .delay(3.2);
-
-gsap
-  .from('.lense-vibrantCommunity', {
-    opacity: 0,
-    duration: 0.5,
-  })
-  .delay(3.6);
-
-gsap
-  .from('.lense-commercialSuccess', {
-    opacity: 0,
-    duration: 0.5,
-  })
-  .delay(4);
-
-gsap
-  .from('.start-text', {
-    duration: 1.3,
-    x: -200,
-    opacity: 0,
-  })
-  .delay(1.3);
+loadScreen1();
 
 // Button click animation
 $('.play-video').on('click', function () {
@@ -77,7 +325,6 @@ $('.play-video').on('click', function () {
   gsap.fromTo('.play-video', { opacity: 1 }, { opacity: 0, duration: 1.1 });
   gsap.fromTo('.video-area', { opacity: 0 }, { display: 'flex', duration: 2, opacity: 1 });
 });
-
 $('#close-video').on('click', function () {
   gsap.to('.start-text', {
     y: 0,
@@ -86,77 +333,3 @@ $('#close-video').on('click', function () {
   gsap.fromTo('.play-video', { opacity: 0, duration: 1.1 }, { opacity: 1 });
   gsap.fromTo('.video-area', { display: 'flex', duration: 2, opacity: 1 }, { opacity: 0 });
 });
-
-// Exit transition
-const pageTransition = () => {
-  gsap.to('.start-text', {
-    duration: 1.3,
-    x: -200,
-    opacity: 0,
-  });
-  gsap.to('.lense-commercialSuccess-back', {
-    zIndex: 5,
-    duration: 0,
-  });
-  gsap.to('.lense-commercialSuccess-back', {
-    opacity: 1,
-    duration: 1.5,
-  });
-  gsap
-    .to(
-      `.lense-positiveSocialChange,
-        .lense-revolutionizeTech,
-        .lense-awesomePartner,
-        .lense-vibrantCommunity,
-        .lense-commercialSuccess,
-        .lenses-core,
-        .background-image,
-        .skeleton`,
-      {
-        duration: 0.5,
-        opacity: 0,
-      },
-    )
-    .delay(0.8);
-  gsap
-    .to('.lense-commercialSuccess-back', {
-      duration: 0.8,
-      left: '-103.5%',
-      width: '8%',
-      top: '6.25%',
-    })
-    .delay(1.3);
-  gsap
-    .to('#screen1', {
-      duration: 1,
-      backgroundColor: '#ECF1F3',
-    })
-    .delay(1.3);
-  gsap
-    .to('#screen1', {
-      duration: 0.1,
-      display: 'none',
-    })
-    .delay(2.3);
-  loadScreen2();
-};
-
-$('.lense-commercialSuccess').on('click', pageTransition);
-
-// Load Screen 2
-const loadScreen2 = () => {
-  document.getElementById('screen2').style.display = 'block';
-  gsap
-    .from('.back-icon-button', {
-      duration: 0.8,
-      opacity: 0,
-    })
-    .delay(2.3);
-  gsap
-    .from('.description', {
-      duration: 1,
-      x: -100,
-      opacity: 0,
-    })
-    .delay(2.3);
-};
